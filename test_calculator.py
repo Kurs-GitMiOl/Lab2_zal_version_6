41256aab703ffb86cd4749fe899e2b03b51567c8
Kurs GitMiOl <mirek_olszowka_git@o2.pl>
2025-11-03 19:29:25 +0100

    Lab2_zal calculator + testy klasy

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,11 @@
 from calculator import Calculator
-import runpy
 
-# Test addition
+
+# Tests addition
 def test_sum_positive_numbers():
     calc_1 = Calculator(10.0, 5.0)
     assert calc_1.sum() == 15.0
+
 
 def test_sum_negative_numbers():
     calc_1 = Calculator(-10.0, -5.0)
@@ -14,29 +15,34 @@ def test_sum_mixed_sign_numbers():
     calc_1 = Calculator(-10.0, 5.0)
     assert calc_1.sum() == -5.0
 
+
 def test_sum_with_zero():
     calc_1 = Calculator(10.0, 0.0)
     assert calc_1.sum() == 10.0
 
-# Test subtraction
+
+# Tests subtraction
 def test_subtract_positive_numbers():
     calc_1 = Calculator(10.0, 5.0)
     assert calc_1.subtract() == 5
+
 
 def test_subtract_negative_numbers():
     calc_1 = Calculator(-5.5, -1.0)
     assert calc_1.subtract() == -4.5
 
+
 def test_subtract_mixed_sign_numbers():
     calc_1 = Calculator(-5.5, 1.0)
     assert calc_1.subtract() == -6.5
+
 
 def test_subtract_with_zero():
     calc_1 = Calculator(7.0, 0.0)
     assert calc_1.subtract() == 7.0
 
 
-# Test multiplication
+# Tests multiplication
 def test_multiply_positive_numbers():
     calc_1 = Calculator(10.0, 5.0)
     assert calc_1.multiply() == 50.0
@@ -45,6 +51,7 @@ def test_multiply_positive_numbers():
 def test_multiply_negative_numbers():
     calc_1 = Calculator(-10.0, -5.0)
     assert calc_1.multiply() == 50.0
+
 
 def test_multiply_mixed_sign_numbers():
     calc_1 = Calculator(-10.0, 5.0)
@@ -56,7 +63,7 @@ def test_multiply_with_zero():
     assert calc_1.multiply() == 0.0
 
 
-# Test division with correct numbers
+# Tests division with correct numbers
 def test_division_positive_numbers():
     calc_1 = Calculator(10.0, 5.0)
     assert calc_1.divide() == 2.0
@@ -65,6 +72,7 @@ def test_division_positive_numbers():
 def test_division_negative_numbers():
     calc_1 = Calculator(-10.0, -5.0)
     assert calc_1.divide() == 2.0
+
 
 def test_division_mixed_sign_numbers():
     calc_1 = Calculator(-10.0, 5.0)
